7a6575c75a91dfe42f8509e35796abf1050a5f19
prasoon <trups.son@gmail.com>
2019-12-03 20:13:30 +0530

    initial commit

---
+++ awp/tablegallary.php
@@ -0,0 +1,74 @@
+
+<!DOCTYPE html>
+<html>
+<head> <title>TABLE GALLERY</title>
+</head>
+<body>
+
+	<table align="center" border="2">
+
+		<tr>
+			<td>
+				<figure>
+				<img src="1.jpg" height=100% width=100%>
+				<figcaption> <center> suzuki</figcaption>
+				</figure>
+		 	</td>
+			<td>
+				<figure>
+				<img src="1.jpg" height=100% width=100%>
+				<figcaption> <center> suzuki</figcaption>
+				</figure>
+		 	</td>
+			<td>
+				<figure>
+				<img scr="1.jpg" width=100% hight=100%>
+				<figcaption> <center> suzuki</figcaption>
+				</figure>
+		 	</td>
+		</tr>	
+		<tr>
+			<td>
+				<figure>
+				<img scr="1.jpg" width=100% hight=100%>
+				<figcaption> <center> suzuki</figcaption>
+				</figure>
+		 	</td>
+			<td>
+				<figure>
+				<img scr="1.jpg" width=100% hight=100%>
+				<figcaption> <center> suzuki</figcaption>
+				</figure>
+		 	</td>
+			<td>
+				<figure>
+				<img scr="1.jpg" width=100% hight=100%>
+				<figcaption> <center> suzuki</figcaption>
+				</figure>
+		 	</td>
+		</tr>
+		<tr>
+			<td>
+				<figure>
+				<img scr="1.jpg" width=100% hight=100%>
+				<figcaption> <center> suzuki</figcaption>
+				</figure>
+		 	</td>
+			<td>
+				<figure>
+				<img scr="1.jpg" width=100% hight=100%>
+				<figcaption> <center> suzuki</figcaption>
+				</figure>
+		 	</td>
+			<td>
+				<figure>
+				<img scr="1.jpg" width=100% hight=100%>
+				<figcaption> <center> suzuki</figcaption>
+				</figure>
+		 	</td>
+		</tr>	
+	</table>
+
+</body>
+
+</html>
